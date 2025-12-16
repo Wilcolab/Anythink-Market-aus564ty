48e408c6fd4eaf8741738c870abd377a3793bf23
WilcoApp <hello@wilco.gg>
2025-12-16 18:43:54 +0000

    Initial commit

--- few_shot_prompt.js
+++
@@ -1,4 +0,0 @@
-// Few-shot Prompt: Write a JavaScript function called toCamelCase that converts strings to camelCase.
-// Example 1: toCamelCase('hello_world') should return 'helloWorld'
-// Example 2: toCamelCase('user-name') should return 'userName'
-// Example 3: toCamelCase('first name') should return 'firstName'
